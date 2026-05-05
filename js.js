b9e903773656b10dd7b51bf9f61f4cfea3a0728b
Rafael Fernandes <rafashow22@outlook.com>
2026-05-04 22:18:12 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -44,3 +44,25 @@ document.getElementById('panel-left')
 
 document.getElementById('panel-right')
   .addEventListener('mouseleave', resetLean);
+
+  function lockScroll() {
+
+    const scrollY = window.scrollY;
+
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${scrollY}px`;
+    document.body.style.width = '100%';
+
+}
+
+function unlockScroll() {
+
+    const scrollY = document.body.style.top;
+
+    document.body.style.position = '';
+    document.body.style.top = '';
+    document.body.style.width = '';
+
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
+}
